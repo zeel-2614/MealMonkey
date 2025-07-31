eddeba2a86127e3a49922a71f9b8cc1a01fceda0
Zeel Shah <zeel.shah@brainvire.com>
2025-07-31 19:07:27 +0530

    Created a new app

--- FoodDeliveryApp/Module/Splash/ViewController/SplashViewController.swift
+++ FoodDeliveryApp/Module/Splash/ViewController/SplashViewController.swift
@@ -28,16 +28,4 @@ class SplashViewController: UIViewController {
             }
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
